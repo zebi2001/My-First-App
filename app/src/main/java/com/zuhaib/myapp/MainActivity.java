8d6f6693f0bedc0e85a346b62b74495f8d9c032c
zebi2001 <zuhaibrockstar419@gmail.com>
2022-02-13 16:32:12 +0500

    My First App

---
+++ app/src/main/java/com/zuhaib/myapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.zuhaib.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.btn_submit1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openactivity();
+            }
+        });
+    }
+    public void openactivity(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+
+    }
+}
